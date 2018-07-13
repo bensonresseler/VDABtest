c09edb5dfb24c20d98cd2dbcf925e3c00409c2d5
bensonresseler <bresseler.vdab@gmail.com>
2018-07-13 11:14:54 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Geef een naam: ");
+            String naam = scanner.nextLine();
+            System.out.println(String.format("Naam: %10s", naam));
+        }
+
+    }
+}
