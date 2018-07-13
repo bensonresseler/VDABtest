88b6aa116f62d92587c79a4b4a210220ee12e2e4
bensonresseler <bresseler.vdab@gmail.com>
2018-07-13 16:05:29 +0200

    V03, uitbreiding

--- src/com/company/Bestemming.java
+++ src/com/company/Bestemming.java
@@ -8,17 +8,25 @@ public class Bestemming {
 
         String [] bestemmingen = {"Corsica", "Sicilië", "Sardinië", "Montenegro"};
 
-
         System.out.print("Geef je naam: ");
         String naam = scanner.nextLine();
+        int keuze = 1;
 
-        System.out.println("Kies een bestemming:");
+        do {
+            if (keuze != 1){
+                System.out.println("Kies een geldig nummer.");
+            }
+            System.out.println("Kies een bestemming:");
 
-        for (int i = 0; i < bestemmingen.length; i++) {
-            System.out.printf("\t %d. %s \n",i+1, bestemmingen[i]);
-        }
-        System.out.print("Welke bestemming kies je (1-3)? ");
-        int keuze = Integer.parseInt(scanner.nextLine());
+            for (int i = 0; i < bestemmingen.length; i++) {
+                System.out.printf("\t %d. %s \n",i+1, bestemmingen[i]);
+            }
+
+            System.out.printf("Welke bestemming kies je (1-%d)? ", bestemmingen.length);
+            keuze = Integer.parseInt(scanner.nextLine());
+
+        } while (keuze < 1 || keuze >= bestemmingen.length + 1);
+
         System.out.printf("Dag %s, goede reis naar %s.", naam, bestemmingen[keuze-1]);
     }
 }
